bf8c5c144048af32649626b0cc33d847885e1489
Maria Osipova <sesterca.mariaosipova@yandex.ru>
2022-05-10 20:13:22 +0300

    add screenshot, page source, console logs attachments

--- src/test/java/com/demoqa/BaseTest.java
+++ src/test/java/com/demoqa/BaseTest.java
@@ -1,13 +1,22 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.demoqa.helpers.Attachments;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
 
     @BeforeAll
-    public void config(){
+    static void config(){
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments(){
+        Attachments.screenshotAs("Screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
     }
 }
