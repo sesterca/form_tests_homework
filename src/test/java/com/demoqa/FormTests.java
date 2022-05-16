89ca0e9f1c172779d23de0ae7d17c0db239729b1
Maria Osipova <sesterca.mariaosipova@yandex.ru>
2022-05-17 00:17:04 +0300

    change tasks

--- src/test/java/com/demoqa/FormTests.java
+++ src/test/java/com/demoqa/FormTests.java
@@ -15,7 +15,6 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-@Tag("systemProperties")
 public class FormTests extends BaseTest{
 
     private FakerTestData student;
@@ -55,11 +54,10 @@ public class FormTests extends BaseTest{
         yearOfBirthday = studentMap.get("Birthday year");
     }
 
-
+    @Tag("demoqa")
     @Severity(SeverityLevel.CRITICAL)
     @DisplayName("Student's successful registration")
     @Test
-    @Tag("demoqa")
     public void automaticPracticeFormPageObjectTest(){
 
         AutomationPracticeForm form = open(AutomationPracticeForm.FORM_PAGE, AutomationPracticeForm.class)
